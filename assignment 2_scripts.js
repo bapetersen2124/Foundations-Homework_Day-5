da9659cc56808e715be28cd8a5744a602141f421
bapetersen2124 <bapetersen2124@gmail.com>
2017-02-28 22:20:46 -0500

    Assignment 2
    Assignment 2 of Javascript exercises 2/28

---
+++ assignment 2_scripts.js
@@ -0,0 +1,11 @@
+function yell(input) {
+  return input.toUpperCase();
+}
+
+console.log(yell("this is my inside voice"));
+
+function yell10(input) {
+  return (input.toUpperCase().repeat(10));
+}
+
+console.log(yell10("this is my inside voice "));
