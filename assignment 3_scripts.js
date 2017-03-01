147168634c4534e7d8c20f230d991656fb66b5d5
bapetersen2124 <bapetersen2124@gmail.com>
2017-02-28 22:53:50 -0500

    Assignment 3
    Completed assignment 3 for 2/28

---
+++ assignment 3_scripts.js
@@ -0,0 +1,11 @@
+function longest(input1, input2) {
+  var length1=input1.length;
+  var length2=input2.length;
+  if (length1 > length2) {
+    console.log(input1);
+  } else {
+    console.log(input2);
+  }
+}
+
+(longest("this is my inside voice","bernesto the besto my favorite little pesto"));
